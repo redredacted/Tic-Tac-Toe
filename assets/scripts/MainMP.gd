bd89cba0774e74fb9f750f7b10160e32b0cddb13
Kenny Sweet <redredacted@gmail.com>
2019-04-08 16:58:03 -0500

    -fixed issues with multiplayer mode
    Signed-off-by: Kenny Sweet <redredacted@gmail.com>

--- assets/scripts/MainMP.gd
+++ assets/scripts/MainMP.gd
@@ -1,38 +1,45 @@
 extends "res://assets/scripts/Main.gd"
 
 var turn : bool = false
+var peer : NetworkedMultiplayerENet
+var client : int
 
 func _on_HostBtn_pressed():
-	var peer = NetworkedMultiplayerENet.new()
+	peer = NetworkedMultiplayerENet.new()
+#warning-ignore:return_value_discarded
 	peer.create_server(27015, 1)
 	get_tree().set_network_peer(peer)
 	get_tree().set_meta("network_peer", peer)
 	$MPMenu.hide()
+#warning-ignore:return_value_discarded
 	peer.connect("peer_connected", self, "_on_Player_connected")
+#warning-ignore:return_value_discarded
 	peer.connect("peer_disconnected", self, "_on_Player_disconnected")
 	turn = true
 
 func _on_ConnectBtn_pressed():
-	var peer = NetworkedMultiplayerENet.new()
+	peer = NetworkedMultiplayerENet.new()
+#warning-ignore:return_value_discarded
 	peer.create_client($MPMenu/IPEdit.text, 27015)
 	get_tree().set_network_peer(peer)
 	get_tree().set_meta("network_peer", peer)
 	$MPMenu.hide()
+#warning-ignore:return_value_discarded
 	peer.connect("connection_succeeded", self, "_on_connection_succeeded")
+#warning-ignore:return_value_discarded
 	peer.connect("connection_failed", self, "_on_connection_failed")
+#warning-ignore:return_value_discarded
 	peer.connect("server_disconnected", self, "_on_disconnected")
 	turn = false
 
 func respondInput(pos, piece):
 	var row = brd.screenToBoard(pos.x)
 	var col = brd.screenToBoard(pos.y)
-	brd.placePiece(row, col, piece)
-	turn = false
 	rpc("update_board", row, col, piece)
 
-remote func update_board(row, col, piece):
+remotesync func update_board(row, col, piece):
 	brd.placePiece(row, col, piece)
-	turn = true
+	turn = !turn
 
 func _input(event):
 	if event is InputEventMouseButton && event.pressed && turn:
@@ -43,37 +50,46 @@ func _input(event):
 			else:
 				respondInput(event.position, brd.Piece.O)
 
-remote func ready_up():
+remotesync func ready_up():
 	brd.show();
 
 #serverside events
 func _on_Player_connected(id):
 	print("Player, " + str(id) + " connected")
+	client = id
 	rpc("ready_up")
-	ready_up()
 
 func _on_Player_disconnected(id):
+	_on_MainMenu_pressed()
 	print("Player, " + str(id) + " disconnected")
-	$MPMenu.show()
-	brd.hide()
-	get_tree().set_network_peer(null)
 
 #client events
 func _on_connection_succeeded():
 	print("Connected")
 
 func _on_connection_failed():
+	_on_MainMenu_pressed()
 	print("Connection Failed")
-	$MPMenu.show()
-	brd.hide()
 
 func _on_disconnected():
+	_on_MainMenu_pressed()
 	print("Disconected")
-	$MPMenu.show()
-	brd.hide()
-	get_tree().set_network_peer(null)
 
 func _game_over(winner):
-	$UI/GridContainer/NewGame.hide()
+	#$UI/GridContainer/NewGame.hide()
 	._game_over(winner)
+
+remotesync func restart():
+	if get_tree().is_network_server():
+		turn = true
+	else:
+		turn = false
 	
+	._restartGame()
+
+func _on_NewGame_pressed():
+	rpc("restart")
+
+func _on_MainMenu_pressed():
+	peer.close_connection(10)
+	._on_MainMenu_pressed()
